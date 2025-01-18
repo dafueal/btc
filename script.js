ffd98cfc2ec31c3ee43d6b416352b51737881e87
dafueal <39571728+dafueal@users.noreply.github.com>
2025-01-18 19:01:06 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-const btcPriceElement = document.getElementById('osmo-price');
+const btcPriceElement = document.getElementById('btc-price');
+
+async function fetchBTCPrice() {
+    try {
+        const response = await fetch('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT');
+        const data = await response.json();
+        const price = parseFloat(data.price).toFixed(2);
+        btcPriceElement.innerText = `BTC: $${price}`;
+    } catch (error) {
+        btcPriceElement.innerText = 'Error fetching price';
+        console.error('Error fetching BTC price:', error);
+    }
+}
 
 async function fetchOSMOPrice() {
     try {
-        const response = await fetch('https://api.osmosis.zone/v1/prices/osmo'); // Updated to use Osmosis API
+        const response = await fetch('https://api.osmosis.zone/v1/price/osmo');
         const data = await response.json();
         const osmoPrice = parseFloat(data.price).toFixed(2); // Format the price
         document.getElementById('osmo-price').innerText = `OSMO: $${osmoPrice}`; // Update HTML element
         console.log(`OSMO Price: ${osmoPrice} USDT`);
     } catch (error) {
-        document.getElementById('osmo-price').innerText = 'Error fetching OSMO price';
         console.error('Error fetching OSMO price:', error);
     }
 }
 
-async function fetchWBTCPrice() {
-    try {
-        const response = await fetch('https://api.osmosis.zone/v1/prices/wbtc'); // Updated to use Osmosis API for WBTC
-        const data = await response.json();
-        const wbtcPrice = parseFloat(data.price).toFixed(2); // Format the price
-        document.getElementById('wbtc-price').innerText = `WBTC: $${wbtcPrice}`; // Update HTML element
-        console.log(`WBTC Price: ${wbtcPrice} USDT`);
-    } catch (error) {
-        document.getElementById('wbtc-price').innerText = 'Error fetching WBTC price';
-        console.error('Error fetching WBTC price:', error);
-    }
-}
-
+setInterval(fetchBTCPrice, 2000); // Update every second
 setInterval(fetchOSMOPrice, 2000); // Update every second
-setInterval(fetchWBTCPrice, 2000); // Update every second
-fetchOSMOPrice(); // Initial fetch
-fetchWBTCPrice(); // Initial fetch
+fetchBTCPrice(); // Initial fetch
+fetchOSMOPrice();
